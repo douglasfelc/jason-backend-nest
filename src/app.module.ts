3adcdd0ad6ffd6591e1086326a1789b6d0b29236
Douglas Felchilcher <douglasfelc@gmail.com>
2023-08-17 06:31:13 -0300

    Generate resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EpicsModule } from './epics/epics.module';
+import { TasksModule } from './tasks/tasks.module';
+import { LabelsModule } from './labels/labels.module';
+import { ProjectsModule } from './projects/projects.module';
+import { StatusModule } from './status/status.module';
+import { VersionsModule } from './versions/versions.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [EpicsModule, TasksModule, LabelsModule, ProjectsModule, StatusModule, VersionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
